d032ab22cd9653729612cc1644902bf5628b6b0d
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-04-25 01:12:19 +0200

    finished login logic

--- Assignment_2/admim-panel/src/routes/index.svelte
+++ Assignment_2/admim-panel/src/routes/index.svelte
@@ -9,8 +9,6 @@
 	let userInputName: string;
 	let userInputPassword: string;
 	let loginAttempts: number = 0;
-	$: console.log(userInputName);
-	$: console.log(userInputPassword);
 
 	function attempLogin(credentials: Credentials) {
 		if (credentials.username === userInputName && credentials.password === userInputPassword) {
@@ -23,37 +21,49 @@
 	}
 </script>
 
-<div class="grid h-screen">
-	<div class="grid max-w-xs grid-cols-2 gap-4 place-self-center rounded-md border-2 p-4">
-		<div
-			on:click={() => (adminSelected = false)}
-			class="basis-1/2 cursor-pointer rounded-md bg-blue-300 
-            {adminSelected ? 'bg-blue-300 hover:bg-blue-400' : 'bg-blue-500'} p-2 text-center">
-			Staff Login
-		</div>
-		<div
-			on:click={() => (adminSelected = true)}
-			class="basis-1/2 cursor-pointer rounded-md 
-            {adminSelected ? 'bg-blue-500' : 'bg-blue-300 hover:bg-blue-400'} p-2 text-center">
-			Admin Login
-		</div>
-		<input
-			type="text"
-			name="username"
-			placeholder="Username"
-			bind:value={userInputName}
-			class="col-span-2 rounded-md" />
-		<input
-			type="password"
-			name="password"
-			placeholder="Password"
-			bind:value={userInputPassword}
-			class="col-span-2 rounded-md" />
-		<input
-			on:click={() =>
-				adminSelected ? attempLogin(adminCredentials) : attempLogin(staffCredentials)}
-			type="button"
-			value="Log In"
-			class=" col-span-2 cursor-pointer rounded-md bg-blue-300 p-2 hover:bg-blue-400" />
-	</div>
+<div class="grid h-screen place-items-center">
+	<form
+		on:submit|preventDefault={() =>
+			attempLogin(adminSelected ? adminCredentials : staffCredentials)}>
+		<fieldset
+			disabled={loginAttempts > 2}
+			class="grid max-w-xs grid-cols-2 gap-4 rounded-md border-2 p-4">
+			<div
+				on:click={() => (adminSelected = false)}
+				class="basis-1/2 cursor-pointer rounded-md p-2 text-center
+            	{adminSelected ? '' : 'bg-blue-500'}
+				{loginAttempts > 2 ? ' cursor-not-allowed bg-slate-300' : 'bg-blue-300  hover:bg-blue-400'}">
+				Staff Login
+			</div>
+			<div
+				on:click={() => (adminSelected = true)}
+				class="basis-1/2 cursor-pointer rounded-md p-2 text-center
+            	{adminSelected ? 'bg-blue-500' : ''}
+				{loginAttempts > 2 ? ' cursor-not-allowed bg-slate-300' : 'bg-blue-300  hover:bg-blue-400'}">
+				Admin Login
+			</div>
+			<input
+				type="text"
+				name="username"
+				required
+				placeholder="Username"
+				bind:value={userInputName}
+				class="col-span-2 rounded-md" />
+			<input
+				type="password"
+				name="password"
+				required
+				placeholder="Password"
+				bind:value={userInputPassword}
+				class="col-span-2 rounded-md" />
+			<input
+				type="submit"
+				value="Log In"
+				class=" col-span-2 cursor-pointer rounded-md p-2
+				{loginAttempts > 2 ? ' cursor-not-allowed bg-slate-300' : 'bg-blue-300  hover:bg-blue-400'}" />
+		</fieldset>
+		{#if loginAttempts > 2}
+			<div class="text-center text-red-500">Too many login attempts.</div>
+		{/if}
+	</form>
 </div>
