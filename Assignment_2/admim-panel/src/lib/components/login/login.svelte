f65f975d641a95b6302c44af49ade842f27a15d8
Vincent Dlugosch <dlugoschvincent@gmail.com>
2022-04-27 14:11:13 +0200

    added static adapter and moved login to seperate component

---
+++ Assignment_2/admim-panel/src/lib/components/login/login.svelte
@@ -0,0 +1,96 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+
+	enum UserGroup {
+		ADMIN = 'admin',
+		STAFF = 'staff'
+	}
+
+	class User {
+		username: string;
+		password: string;
+		group: UserGroup;
+		constructor(username: string, password: string, group: UserGroup) {
+			this.username = username;
+			this.password = password;
+			this.group = group;
+		}
+		validate(username: string, password: string, group: UserGroup) {
+			return this.username === username && this.password === password && this.group === group;
+		}
+	}
+
+	const users = [
+		new User('Admin', 'Admin', UserGroup.ADMIN),
+		new User('Staff', 'Staff', UserGroup.STAFF)
+	];
+
+	let loginAttempts: number = 0;
+
+	function submitForm(event: SubmitEvent) {
+		const formData = new FormData(event.target as HTMLFormElement);
+		const userName = formData.get('username') as string;
+		const userPassword = formData.get('password') as string;
+		const userGroup = formData.get('userGroup') as UserGroup;
+
+		for (let user of users) {
+			if (user.validate(userName, userPassword, userGroup)) {
+				loginAttempts = 0;
+				console.log('Login successful');
+				goto(`/${user.group}`);
+				return;
+			}
+		}
+
+		console.log('Login unsuccessful');
+		loginAttempts++;
+	}
+</script>
+
+<div class="grid h-screen place-items-center">
+	<form on:submit|preventDefault={(event) => submitForm(event)}>
+		<fieldset
+			disabled={loginAttempts > 2}
+			class="grid max-w-xs grid-cols-2 gap-4 rounded-md border-2 p-4">
+			<label class="col-span-1">
+				<input
+					checked
+					type="radio"
+					class="peer hidden"
+					name="userGroup"
+					value={UserGroup.STAFF}
+					required />
+				<div
+					class="cursor-pointer rounded-md bg-blue-300 p-2 text-center hover:bg-blue-400 peer-checked:bg-blue-500 peer-disabled:cursor-not-allowed peer-disabled:bg-slate-300">
+					Staff Login
+				</div>
+			</label>
+			<label class="col-span-1">
+				<input type="radio" class="peer hidden" name="userGroup" value={UserGroup.ADMIN} required />
+				<div
+					class="cursor-pointer rounded-md bg-blue-300 p-2 text-center hover:bg-blue-400 peer-checked:bg-blue-500 peer-disabled:cursor-not-allowed peer-disabled:bg-slate-300">
+					Admin Login
+				</div>
+			</label>
+			<input
+				type="text"
+				name="username"
+				required
+				placeholder="Username"
+				class="col-span-2 rounded-md" />
+			<input
+				type="password"
+				name="password"
+				required
+				placeholder="Password"
+				class="col-span-2 rounded-md" />
+			<input
+				type="submit"
+				value="Log In"
+				class=" col-span-2 cursor-pointer rounded-md bg-blue-300 p-2 hover:bg-blue-400 disabled:cursor-not-allowed disabled:bg-slate-300" />
+		</fieldset>
+		{#if loginAttempts > 2}
+			<div class="text-center text-red-500">Too many login attempts.</div>
+		{/if}
+	</form>
+</div>
--- Assignment_2/admim-panel/svelte.config.js
+++ Assignment_2/admim-panel/svelte.config.js
@@ -1,15 +1,36 @@
-import adapter from '@sveltejs/adapter-auto';
+// import adapter from '@sveltejs/adapter-auto';
+// import preprocess from 'svelte-preprocess';
+
+// /** @type {import('@sveltejs/kit').Config} */
+// const config = {
+// 	// Consult https://github.com/sveltejs/svelte-preprocess
+// 	// for more information about preprocessors
+// 	preprocess: preprocess(),
+
+// 	kit: {
+// 		adapter: adapter()
+// 	}
+// };
+
+// export default config;
+
+// static adapter
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
+export default {
 	preprocess: preprocess(),
-
 	kit: {
-		adapter: adapter()
+		adapter: adapter({
+			// default options are shown
+			pages: 'build',
+			assets: 'build',
+			fallback: null,
+			precompress: false
+		}),
+		prerender: {
+			// This can be false if you're using a fallback (i.e. SPA mode)
+			default: true
+		}
 	}
 };
-
-export default config;
